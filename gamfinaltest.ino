238ccc38e43a6c4b0eeed405309dce56ddaf3a0f
RowanCollege <hallerr@oregonstate.edu>
2024-06-10 01:31:41 -0700

    Update gamfinaltest.ino

--- gamfinaltest.ino
+++ gamfinaltest.ino
@@ -3,15 +3,35 @@
 // Define game variables
 int score = 0;
 const int maxCommands = 10;
-char commands[maxCommands];
+char commands[maxCommands]; // using characters becasue they are new and make it easy to call to later.
 unsigned long commandStartTime;
-const unsigned long commandTimeout = 2000; // 2 seconds for each command
-int soundP = 75;
-
+const unsigned long commandTimeout = 2000; 
+int soundP = 85;
+int timeDropNum = 100;
+int timeDrop = commandTimeout;
+bool endlessMode = false;
+bool prevSlideSwitchState = CircuitPlayground.slideSwitch(); // so slide switch reads a change 
 void setup() {
   CircuitPlayground.begin();
   randomSeed(analogRead(0)); // Seed the random number generator
   Serial.begin(9600);
+  while (!CircuitPlayground.leftButton() && !CircuitPlayground.rightButton()) { //showing which button is endless or 10 game count via leds
+        CircuitPlayground.setPixelColor(0, 255, 0, 0);
+        CircuitPlayground.setPixelColor(1, 255, 0, 0);
+        CircuitPlayground.setPixelColor(2, 255, 0, 0);
+        CircuitPlayground.setPixelColor(3, 255, 0, 0);
+        CircuitPlayground.setPixelColor(4, 255, 0, 0);
+        CircuitPlayground.setPixelColor(5, 255, 0, 255);
+        CircuitPlayground.setPixelColor(6, 255, 0, 255);
+        CircuitPlayground.setPixelColor(7, 255, 0, 255);
+        CircuitPlayground.setPixelColor(8, 255, 0, 255);
+        CircuitPlayground.setPixelColor(9, 255, 0, 255);
+
+        delay(10); 
+    }
+
+    // Determine mode based on button press
+    endlessMode = CircuitPlayground.rightButton();
 }
 
 void loop() {
@@ -20,41 +40,62 @@ void loop() {
 
 void startGame() {
   score = 0;
-  for (int i = 0; i < maxCommands; i++) {
-    generateCommand(i);
-    if (!performCommand(commands[i])) {
-      gameOver();
-      return;
-    }
-    score++;
-    indicateScore(score);
-  }
-  winGame();
-}
+  if (!endlessMode) { // 10 game mode (from the first snippet)
+        for (int i = 0; i < maxCommands; i++) {
+            generateCommand(i);
+            if (!performCommand(commands[i])) {
+                gameOver();
+                return;
+            }
+            score++;
+            indicateScore(score);
+        }
+        winGame();
+    } 
+    else { 
+  int loopCount = endlessMode ? 0 : maxCommands;  // Loop indefinitely 
 
-void generateCommand(int index) {
-  int commandType = random(0, 3);
+    do {
+        generateCommand(score); // Use score as index for endless mode makes code simpler
+        if (!performCommand(commands[score % maxCommands])) { // Wrap around for endless mode
+            gameOver();
+            return;
+        }
+        score++;
+        indicateScore(score);
+    } while (endlessMode || loopCount++ < maxCommands); // just to keep track of loops 
+
+    winGame();
+}
+}
+void generateCommand(int index) { // indez commands to call to them later in the switch case
+  int commandType = random(0, 5);
   switch (commandType) {
     case 0:
       commands[index] = 'L'; // Left button
+      timeDrop = timeDrop - timeDropNum; //make it progressivley harder 
       break;
     case 1:
       commands[index] = 'R'; // Right button
+      timeDrop = timeDrop - timeDropNum;
       break;
     case 2:
       commands[index] = 'S'; // Slide switch
+      timeDrop = timeDrop - timeDropNum;
       break;
     case 3:
       commands[index] = 'M'; // Microphone
+      timeDrop = timeDrop - timeDropNum;
       break;
     case 4:
       commands[index] = 'A'; // Accelerometer
+      timeDrop = timeDrop - timeDropNum;
       break;
   }
   indicateCommand(commands[index]);
 }
 
-bool performCommand(char command) {
+bool performCommand(char command) { // this was split out as a suggestions i found it allowed me to index earlier and then call again here for the actual function.
   commandStartTime = millis();
   switch (command) {
     case 'L':
@@ -62,7 +103,7 @@ bool performCommand(char command) {
     case 'R':
       return waitForRightButton();
     case 'S':
-      return waitForSlideSwitch();
+      return waitForSlideSwitchChange();
     case 'M':
       return waitForClap();
     case 'A':
@@ -72,8 +113,8 @@ bool performCommand(char command) {
   }
 }
 
-bool waitForLeftButton() {
-  while (millis() - commandStartTime < commandTimeout) {
+bool waitForLeftButton() { // waits for button
+  while (millis() - commandStartTime < timeDrop) {
     if (CircuitPlayground.leftButton()) {
       playSuccessSound();
       return true;
@@ -84,7 +125,7 @@ bool waitForLeftButton() {
 }
 
 bool waitForRightButton() {
-  while (millis() - commandStartTime < commandTimeout) {
+  while (millis() - commandStartTime < timeDrop) {
     if (CircuitPlayground.rightButton()) {
       playSuccessSound();
       return true;
@@ -94,9 +135,11 @@ bool waitForRightButton() {
   return false;
 }
 
-bool waitForSlideSwitch() {
-  while (millis() - commandStartTime < commandTimeout) {
-    if (CircuitPlayground.slideSwitch()) {
+bool waitForSlideSwitchChange() { // check for  change in the slide switch
+  while (millis() - commandStartTime < timeDrop) {
+    bool currentSlideSwitchState = CircuitPlayground.slideSwitch();
+    if (currentSlideSwitchState != prevSlideSwitchState) {
+      prevSlideSwitchState = currentSlideSwitchState;
       playSuccessSound();
       return true;
     }
@@ -106,10 +149,10 @@ bool waitForSlideSwitch() {
 }
 
 bool waitForClap() {
-  float soundLevel = CircuitPlayground.mic.soundPressureLevel(10);
-  Serial.println(soundLevel);
-  while (millis() - commandStartTime < commandTimeout) {
-      if (soundLevel > soundP) {
+  while (millis() - commandStartTime < timeDrop) {
+    float soundLevel = CircuitPlayground.mic.soundPressureLevel(10);
+    Serial.println(soundLevel);
+    if (soundLevel > soundP) {
       playSuccessSound();
       return true;
     }
@@ -119,8 +162,8 @@ bool waitForClap() {
 }
 
 bool waitForShake() {
-  while (millis() - commandStartTime < commandTimeout) {
-    if (CircuitPlayground.motionX() > 1.5 || CircuitPlayground.motionY() > 1.5 || CircuitPlayground.motionZ() > 1.5) {
+  while (millis() - commandStartTime < timeDrop) {
+    if (CircuitPlayground.motionX() > 17.5 || CircuitPlayground.motionY() > 17.5 || CircuitPlayground.motionZ() > 17.5) { // would have used total accelration but found this adn decided to try it it is less code so :^)
       playSuccessSound();
       return true;
     }
@@ -154,17 +197,18 @@ void indicateCommand(char command) {
 void indicateScore(int score) {
   CircuitPlayground.clearPixels();
   for (int i = 0; i < score; i++) {
-    CircuitPlayground.setPixelColor(i, 0, 255, 0); // Green for score
+    CircuitPlayground.setPixelColor(i, 55, 55, 55); // White for score going around the board doesn't do anything past ten on endless
   }
   CircuitPlayground.strip.show();
+  delay(500);
 }
 
 void playSuccessSound() {
-  CircuitPlayground.playTone(1000, 200); // High tone
+  CircuitPlayground.playTone(1000, 200); 
 }
 
 void playFailSound() {
-  CircuitPlayground.playTone(200, 200); // Low tone
+  CircuitPlayground.playTone(200, 200);
 }
 
 void gameOver() {
@@ -173,7 +217,8 @@ void gameOver() {
     CircuitPlayground.setPixelColor(i, 255, 0, 0); // Red for game over
   }
   CircuitPlayground.strip.show();
-  CircuitPlayground.playTone(200, 500); // Long low tone
+  timeDrop = commandTimeout;
+  CircuitPlayground.playTone(200, 500); 
   delay(1000);
 }
 
@@ -183,6 +228,7 @@ void winGame() {
     CircuitPlayground.setPixelColor(i, 0, 0, 255); // Blue for win
   }
   CircuitPlayground.strip.show();
-  CircuitPlayground.playTone(1000, 500); // Long high tone
+  CircuitPlayground.playTone(1000, 500); 
+  timeDrop = commandTimeout;
   delay(1000);
 }
